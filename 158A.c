cc6cb88725285cdcb14c8023a1b40edc37c9f1cf
Shreenabh Agrawal <me@shreenabh.com>
2022-01-07 22:49:12 +0530

    Create 158A.c

---
+++ 158A.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    int k;
+    scanf("%d %d", &n, &k);
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    int cutoff = a[k - 1];
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] >= cutoff && a[i] != 0)
+        {
+            count++;
+        }
+    }
+    printf("%d", count);
+}
